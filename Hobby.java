574c4eeffc52b209feb1ecd695179a1f4c9a07ce
ksschemmel <112411813+ksschemmel@users.noreply.github.com>
2022-08-29 11:21:18 -0400

    Add files via upload

---
+++ Hobby.java
@@ -0,0 +1,11 @@
+//This is the public class called Hobby
+public class Hobby
+{
+//We are writing a Hobby program
+public static void main(String[] args)
+{
+//
+System.out.println("photography");
+}
+
+}

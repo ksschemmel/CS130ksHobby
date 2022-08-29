7a82f6cf100a34188de52dbbe4ddfb430f3ca9cc
ksschemmel <112411813+ksschemmel@users.noreply.github.com>
2022-08-29 12:00:58 -0400

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum
+{
+public static void main(String[] args)
+{
+int number1 = 3;
+int number2 = 5;
+int sum = number1 + number2;
+System.out.println(sum);
+int multiplication = number1 * number2;
+System.out.println(multiplication);
+int subtraction = number1-number2;
+System.out.println(subtraction);
+}
+}
